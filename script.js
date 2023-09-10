7523349c0d40076e658db7f43fbf62c492326339
ray-sarthak <117006899+ray-sarthak@users.noreply.github.com>
2023-09-10 23:23:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 window.promises = [];
+	const promise1 = Promise.reject(0);
+const promise2 = new Promise((resolve) => setTimeout(resolve, 100, 'quick'));
+const promise3 = new Promise((resolve) => setTimeout(resolve, 500, 'slow'));
+const promise4 = new Promise((resolve) => setTimeout(resolve, 500, 'slow'));
+const promise5 = new Promise((resolve) => setTimeout(resolve, 500, 'slow'));
+const allPromise = [promise1, promise2,promise3,promise4,promise5];
+
+Promise.any(allPromise).then((value) =>{
+	document.getElementById("output").innerHTML=value;
+});
 
 // Do not change the code above this
 // add your promises to the array `promises`
